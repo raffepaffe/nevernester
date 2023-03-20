74f17189b6d298c679e85dac46f32e90333d8f39
raffepaffe <ralf.krakowski@gmail.com>
2023-03-20 16:38:26 +0100

    check if statement over several lines (#3)

--- examples/src/below_five/two.go
+++ examples/src/below_five/two.go
@@ -1,11 +1,23 @@
 package below_four
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func nestedFuncThatIsNested2Levels() {
 	terminator := "Arnold Schwarzenegger"
 	rambo := "Sylvester Stallone"
 	if terminator != rambo {
 		fmt.Println("Terminator and Rambo are not the same")
+	}
+}
+
+func ifNesting2Levels() {
+	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+	var dat map[string]interface{}
+	if err := json.Unmarshal(
+		byt, &dat); err != nil {
+		panic(err)
 	}
 }
